20fa4368202ea5653fbd6fe7b0002efc77f9dd59
dabgan <gabriel.danilukk@gmail.com>
2024-02-07 12:54:21 +0100

    feat: add express middleware

--- middleware/logEvents.js
+++ middleware/logEvents.js
@@ -10,13 +10,18 @@ const logEvents = async (message, logName) => {
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
     console.log(__dirname);
     try {
-        if (!fs.existsSync(path.join(__dirname, 'logs'))) {
-            await fsPromises.mkdir(path.join(__dirname, 'logs'));
+        if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
+            await fsPromises.mkdir(path.join(__dirname, '..', 'logs'));
         }
-        await fsPromises.appendFile(path.join(__dirname, 'logs', logName), logItem);
+        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logName), logItem);
     } catch (error) {
         console.error(error);
     }
 };
 
-module.exports = logEvents;
+const logger = (req, res, next) => {
+    logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'requestsLog');
+    next();
+};
+
+module.exports = { logEvents, logger };
--- server.js
+++ server.js
@@ -1,14 +1,72 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
 
-app.get(`^/$|index.html`, (req, res) => {
+// custom middleware logger
+app.use(logger);
+
+// cross origin resource sharing
+const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+// build in middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static('./public', { root: __dirname }));
+
+app.get(`^/$|index(.html)?`, (req, res) => {
     res.sendFile('./views/index.html', { root: __dirname });
 });
 
-app.get('/new-page.html', (req, res) => {
+app.get(`/new-page(.html)?`, (req, res) => {
     res.sendFile('./views/new-page.html', { root: __dirname });
 });
+
+app.get('/old-page(.html)?', (req, res) => {
+    res.redirect(301, '/new-page');
+});
+
+// chaining route handlers
+const one = (req, res, next) => {
+    console.log('one');
+    next();
+};
+
+const two = (req, res, next) => {
+    console.log('two');
+    next();
+};
+
+const three = (req, res) => {
+    console.log('three');
+    res.send('Finished!');
+};
+
+app.get('/chain(.html)?', [one, two, three]);
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile('./views/404.html', { root: __dirname });
+    } else if (req.accepts('json')) {
+        res.json({ error: '404 Not Found' });
+    } else {
+        res.type('txt').send('404 Not Found');
+    }
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
